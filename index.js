0ff468c2bd6e93075d75308b64a20839dd743214
hamouda0503 <ahmed.dridi04@gmail.com>
2022-02-22 18:04:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const Port = process.env.port || 5000;
+
+mongoose.connect('mongodb+srv://Plantholic:plantholic@plantholic.y97ji.mongodb.net/Plantholic?retryWrites=true&w=majority');
+
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("MongoDB connected");
+});
+
+// middleware 
+
+app.use(express.json());
+const userRoute = require("./routes/user"); 
+
+app.use("/user", userRoute);  
+
+app.route("/").get((req,res)=>res.json("your first restapi 1"));
+
+app.listen(Port, ()=>console.log(`your server is running on port ${Port}`));
---
+++ routes/user.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const User = require("../models/users.model");
+const config = require("../config");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const middleware = require("../middleware");
+
+router.route("/:username").get(middleware.checkToken, (req, res) => {
+    User.findOne({ username: req.params.username }, (err, result) => {
+        if (err) return res.status(500).json({ msg: err });
+        res.json({
+            data: result,
+            username: req.params.username,
+        });
+
+    });
+});
+
+router.route("/login").post((req, res) => {
+    User.findOne({ username: req.body.username }, (err, result) => {
+        if (err) return res.status(500).json({ msg: err });
+        if (result === null) {
+            return res.status(500).json("either Username or password is incorrect");
+        }
+        if (result.password === req.body.password) {
+            // jwt TOken 
+            let token = jwt.sign({ username: req.body.user }, config.key, {
+                expiresIn: "24h",
+            });
+            res.json({
+                token: token,
+                msg: "success"
+            });
+        }
+        else {
+            res.json(403).json("password is incorrect");
+        }
+    });
+});
+
+router.route("/register").post((req, res) => {
+    console.log("inside the register");
+    const user = new User(
+        {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+        }
+    );
+
+    user.save().then(() => {
+        console.log("user registred ");
+        res.status(200).json("ok");
+    })
+        .catch((err) => {
+            res.status(403).json({ msg: err });
+        });
+
+});
+
+router.route("/update/:username").patch(middleware.checkToken,(req, res) => {
+    User.findOneAndUpdate(
+        {
+            username: req.params.username
+        },
+        {
+            $set: {
+                password: req.body.password
+            }
+        },
+        (err, resultat) => {
+            if (err) return res.status(500).json({ msg: err });
+            const msg = {
+                msg: "password successfully updated",
+                username: req.params.username,
+            };
+            return res.json(msg);
+        }
+    );
+});
+
+router.route("/delete/:username").delete(middleware.checkToken,(req, res) => {
+    User.findOneAndDelete({ username: req.params.username }, (err, resultat) => {
+        if (err) return res.status(500).json({ msg: err });
+        const msg = {
+            msg: "username deleted ",
+            username: req.params.username,
+        };
+        return res.json(msg);
+    });
+});
+
+module.exports = router; 
